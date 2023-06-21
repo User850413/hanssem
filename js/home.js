277695d374d5314ec3382b32254ccb06d26e6e3a
User850413 <csyou1228@gmail.com>
2023-06-21 17:27:13 +0900

    230621

---
+++ js/home.js
@@ -0,0 +1,15 @@
+$(window).scroll(function () {
+  var scr = $(this).scrollTop();
+  if (scr > 10) {
+    $(".header").css("background", "#fff");
+    $(".header .gnb a").css("color", "#000");
+    $(".header .search").css("border", "1px solid #ddd");
+    $(".search i").css("color", "#000");
+    $(".header .gnb .logo img").css("filter", "none");
+  } else {
+    $(".header").css("background", "rgba(256, 256, 256, 0)");
+    $(".header .gnb a").css("color", "#fff");
+    $(".search i").css("color", "#fff");
+    $(".header .gnb .logo img").css("filter", "saturate(0%) brightness(1000%)");
+  }
+});
